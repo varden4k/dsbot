8882c95100d00fc831c31c490262d8853ba4fd27
MoSzynDEV <38393607+MoSzynDEV@users.noreply.github.com>
2018-04-15 09:39:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = "/";
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('GOTOWY DO DZIAŁANIA');
+});
+
+client.on('message', msg => {
+  if (msg.content === prefix + 'raz') {
+    msg.channel.sendMessage('harr skurwysyny XD') 
+  	}
+});
+
+const discord = require('discord.js');
+const client = new discord.Client();
+
+client.on('ready', () => {
+        client.user.setActivity('szanty', {type: 'LISTENING'});
 });
 
 client.on('message', message => {
