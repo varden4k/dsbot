14ea07771403952c8783c5153359a7799e19a3f0
MoSzynDEV <38393607+MoSzynDEV@users.noreply.github.com>
2018-04-15 09:55:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const prefix = "/";
 
 client.on('ready', () => {
-    console.log('GOTOWY DO DZIAŁANIA');
+    console.log('LECIMY Z TEMATEM');
 });
 
 client.on('ready', () => {
