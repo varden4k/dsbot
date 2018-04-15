fa8dcd9dda78fcc13480c068c4665c0754ed2f85
MoSzynDEV <38393607+MoSzynDEV@users.noreply.github.com>
2018-04-15 13:30:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "/";
-const cennik = require('./cennik.js');
+const cennik = require('/cennik.js');
 
 
 client.on('ready', () => {
-    console.log(' PLEBS BUILD 7 ')
+    console.log(' PLEBS BUILD 8 ')
     client.user.setUsername("PIRAT");
     client.user.setStatus('dnd');
     client.user.setActivity('szanty', {type: 'LISTENING'});
