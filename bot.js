284c47a1189099b359e9a8e08b8309b8f395653c
MoSzynDEV <38393607+MoSzynDEV@users.noreply.github.com>
2018-04-15 10:13:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const prefix = "/";
 
 client.on('ready', () => {
-    console.log('DZIAŁA TEST 123');
+    console.log('DZIAŁA TEST3123123213123');
     client.user.setActivity('szanty', {type: 'LISTENING'});
 });
 
