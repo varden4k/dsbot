a6208121e1812ed5d0c29cc5aded87a85599934f
MoSzynDEV <38393607+MoSzynDEV@users.noreply.github.com>
2018-04-15 12:58:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,24 +8,6 @@ client.on('ready', () => {
 });
 
 
-client.on('message', message => {
-    if (message.content === prefix + 'raz') {
-        message.channel.send('test');
-      }
-});
-
- client.on('message', msg => {
-  if (message.content === prefix + 'kostka') {
-    var roll = Math.floor(Math.random() * 6) + 1;
-             message.channel.send({
-      embed: {
-        color: 3447003,
-        description: msg.author + ', wylosowałeś: ' + roll,
-        author: {
-          name: '🎲 Kostka 🎲',
-        }
-      }}
-         )}});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN); 
