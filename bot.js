e87f70e32f530fb87ee8a46403e15cb3674e54f4
MoSzynDEV <38393607+MoSzynDEV@users.noreply.github.com>
2018-04-15 12:52:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const prefix = "/";
 
 client.on('ready', () => {
-    console.log('KURLA123');
+    console.log('PLEBS');
     client.user.setActivity('szanty', {type: 'LISTENING'});
 });
 
